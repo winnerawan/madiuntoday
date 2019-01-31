cb760a983d12352848bb8592e85021ebb4c7022a
Winnerawan T <third1337@gmail.com>
2019-01-31 14:14:37 +0700

    change fabric account

--- app/src/androidTest/java/net/winnerawan/madiun/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/net/winnerawan/madiun/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("co.go_aplikasi.ibacol", appContext.packageName)
+        assertEquals("net.winnerawan.madiun", appContext.packageName)
     }
 }
